11707d93c78e023e65e3168f84defd7d7f3b741f
TshireletsoRakoma <156686384+TshireletsoRakoma@users.noreply.github.com>
2024-05-24 10:46:46 +0200

    creates,store,actions files

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+
+import { Store, tallyReducer } from './store2.js';
+import { createAddAction, createSubtractAction, createResetAction } from './actions.js';
+
+// Create a new store using the tallyReducer
+const store = new Store(tallyReducer);
+
+// Subscribe to state changes and log only the count to the console
+store.subscribe(() => {
+    console.log(store.getState().count); // Log only the count property
+});
