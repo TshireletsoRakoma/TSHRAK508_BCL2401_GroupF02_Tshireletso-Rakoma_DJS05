1f74f070bbcd6963eeac46b8cebece11c0a578df
TshireletsoRakoma <156686384+TshireletsoRakoma@users.noreply.github.com>
2024-05-24 10:59:16 +0200

    "Added actions.js file with createAddAction and createSubtractAction functions"

---
+++ actions.js
@@ -0,0 +1,12 @@
+// actions.js
+
+// Factory function to create an ADD action
+export function createAddAction() {
+    return { type: 'ADD' };
+}
+
+// Factory function to create a SUBTRACT action
+export function createSubtractAction() {
+    return { type: 'SUBTRACT' };
+}
+
