48df489f81fa08d46a8b9ee655edc44f8f7fdbc5
TshireletsoRakoma <156686384+TshireletsoRakoma@users.noreply.github.com>
2024-05-24 10:49:55 +0200

    "Added subscribe method to store class"

--- store2.js
+++ store2.js
@@ -9,3 +9,13 @@
         this.state = this.reducer(this.state, action); // Update state using the reducer
         this.listeners.forEach(listener => listener()); // Notify all listeners
     }
+
+        // Method to subscribe to state changes
+        subscribe(listener) {
+            this.listeners.push(listener); // Add the listener to the array
+            return () => {
+                // Return a function to unsubscribe the listener
+                this.listeners = this.listeners.filter(l => l !== listener);
+            };
+        }
+    }
