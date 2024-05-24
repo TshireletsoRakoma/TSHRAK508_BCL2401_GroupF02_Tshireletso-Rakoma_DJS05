8872f108f889c75effdae18ce3b6ec6d7473b0ca
TshireletsoRakoma <156686384+TshireletsoRakoma@users.noreply.github.com>
2024-05-24 10:49:30 +0200

    "Added dispatch method to store2.js"

--- store2.js
+++ store2.js
@@ -3,3 +3,9 @@
     getState() {
         return this.state;
     }
+
+      // Method to dispatch actions and update the state
+      dispatch(action) {
+        this.state = this.reducer(this.state, action); // Update state using the reducer
+        this.listeners.forEach(listener => listener()); // Notify all listeners
+    }
